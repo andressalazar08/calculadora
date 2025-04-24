a33b5a8f6ce212bcde53c7f6f007e3a7cc137f08
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2025-04-24 15:24:03 -0500

    home con link y pagina suma

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ app= Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Aplicación web con flask"
+    return '''
+
+        Aplicación web con flask-afsr
+        <h1>Calculadora</h1>
+        <h3>opciones disponibles</h3>
+        <ul>
+            <li><a href="/suma">Suma</a></li>
+            <li>Resta</li>
+            <li>Multiplicacion</li>
+            <li>División</li>
+        
+        
+        </ul>
+
+'''
+
+@app.route("/suma")
+def ruta_suma():
+    return "página para aplicar la suma"
+
